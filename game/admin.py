b4f91a2ac887687259a5b2edfdf63468c44ac759
Karolis Vycius <k.vycius@gmail.com>
2018-08-12 08:34:02 +0300

    Politician statistics

--- game/admin.py
+++ game/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 
 from django.contrib.admin import ModelAdmin
+from django.db.models import Count
 
-from game.models import GameOverStatistics
+from game.models import GameOverStatistics, Politician
+
+
+@admin.register(Politician)
+class PoliticianAdmin(ModelAdmin):
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(incorrect_count=Count('game_over_correct'))
+
+    list_display = ['full_name', 'photo_url', 'is_male', 'incorrect_count']
+
+    def incorrect_count(self, obj):
+        return obj.incorrect_count
+
+    incorrect_count.admin_order_field = 'incorrect_count'
 
 
 @admin.register(GameOverStatistics)
